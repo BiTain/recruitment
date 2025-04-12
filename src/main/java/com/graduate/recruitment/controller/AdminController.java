1c38c39e958a8801168f8d09badfad932eb42e36
Nguyen Dang Duy <d0763705638@gmail.com>
2025-04-12 15:46:09 +0700

    update

---
+++ src/main/java/com/graduate/recruitment/controller/AdminController.java
@@ -0,0 +1,37 @@
+package com.graduate.recruitment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/ky-nang")
+    public String skill() {
+        return "admin/skill/list";
+    }
+
+    @GetMapping("/danh-muc")
+    public String danhMuc() {
+        return "admin/danh-muc/list";
+    }
+
+    @GetMapping("/nha-truong")
+    public String nhaTruong() {
+        return "admin/nha-truong/list";
+    }
+
+    @GetMapping("/sinh-vien")
+    public String sinhVien() {
+        return "admin/sinh-vien/list";
+    }
+
+    @GetMapping("/doanh-nghiep")
+    public String doanhNghiep() {
+        return "admin/doanh-nghiep/list";
+    }
+
+    @GetMapping("/bai-dang")
+    public String baiDang() {
+        return "admin/bai-dang/list";
+    }
+}
