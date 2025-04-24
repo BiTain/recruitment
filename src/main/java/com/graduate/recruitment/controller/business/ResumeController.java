5e6ba6a5e54151e1b9fe60e46e14bd0ec0c69e9e
Nguyen Dang Duy <d0763705638@gmail.com>
2025-04-24 20:53:41 +0700

    update code4

--- src/main/java/com/graduate/recruitment/controller/business/ResumeController.java
+++ src/main/java/com/graduate/recruitment/controller/business/ResumeController.java
@@ -12,12 +12,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 @AllArgsConstructor
 public class ResumeController {
     private ResumeService resumeService;
+
     @GetMapping("/doanh-nghiep/ho-so")
     public String getAllResume(Model model,
                                @RequestParam(value = "maDoanhNghiep") String maDoanhNghiep,
                                @RequestParam(value = "page", defaultValue = "0") Integer page,
-                               @RequestParam(value = "limit", defaultValue = "8") Integer limit){
-        model.addAttribute("danhSachHoSo",resumeService.getAllResume(maDoanhNghiep, page, limit).getContent());
+                               @RequestParam(value = "limit", defaultValue = "8") Integer limit,
+                               @RequestParam(value = "status", defaultValue = "dang-cho", required = false)
+                               String status
+    ) {
+        if(status.equals("dang-cho")){
+            model.addAttribute("status", "dang-cho");
+        } else if (status.equals("da-thong-qua")) {
+            model.addAttribute("status", "da-thong-qua");
+        }
+        else if (status.equals("da-tu-choi")) {
+            model.addAttribute("status", "da-tu-choi");
+        } else if (status.equals("dang-cho-phong-van")) {
+            model.addAttribute("status", "dang-cho-phong-van");
+        }
+        model.addAttribute("danhSachHoSo", resumeService.getAllResume(maDoanhNghiep, page, limit).getContent());
         return "business/resume/list";
     }
 
