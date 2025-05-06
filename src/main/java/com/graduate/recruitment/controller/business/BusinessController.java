add55aeb3fd15d472b06f4b87d0b8cd8f1a62068
Nguyen Dang Duy <d0763705638@gmail.com>
2025-05-06 16:32:32 +0700

    upload

--- src/main/java/com/graduate/recruitment/controller/business/BusinessController.java
+++ src/main/java/com/graduate/recruitment/controller/business/BusinessController.java
@@ -2,7 +2,6 @@ package com.graduate.recruitment.controller.business;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
