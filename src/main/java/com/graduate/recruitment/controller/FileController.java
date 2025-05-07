208228130d2215fb06e939cd57a66aded3595945
Bao le <baole26072003@gmail.com>
2025-05-07 16:38:25 +0700

    commit 18

---
+++ src/main/java/com/graduate/recruitment/controller/FileController.java
@@ -0,0 +1,29 @@
+package com.graduate.recruitment.controller;
+
+import com.graduate.recruitment.service.FileService;
+import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/files")
+@AllArgsConstructor
+public class FileController {
+
+    private FileService fileService;
+    @GetMapping("/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = fileService.loadFile(filename);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+}
